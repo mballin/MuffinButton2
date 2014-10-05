a91f7f43cfd9a30d8d199685ddf58ae4935b748f
mballin <matt.ballin@gmail.com>
2014-10-04 19:45:13 -0700

    Actual button + counter
    Muffin button now exists! Muffin label pops up when muffins are created.

--- MuffinButton2/ViewController.swift
+++ MuffinButton2/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var muffinsLabel: UILabel!
+    @IBOutlet weak var muffinNumberLabel: UILabel!
+    @IBOutlet weak var muffinButton: UIButton!
+    @IBOutlet weak var baconLabel: UILabel!
+    
+    var muffins = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        makeInvisible(muffinsLabel)
+        makeInvisible(baconLabel)
     }
 
+    @IBAction func addMuffin(sender: AnyObject) {
+        muffins++
+        makeVisible(muffinsLabel)
+        updateLabels()
+        
+        
+    }
+    
+    func updateLabels(){
+        if muffins>0{
+            muffinsLabel.enabled=1
+        }
+        muffinNumberLabel.text = String(muffins)
+    }
+    
+    func makeInvisible(label: UILabel)
+    {
+        label.textColor = UIColor.whiteColor()
+    }
+    
+    func makeInvisible(button: UIButton)
+    {
+        button.enabled=0
+    }
+    
+    func makeVisible(label: UILabel)
+    {
+        label.textColor = UIColor.blackColor()
+    }
+    
+    func makeVisible(button: UIButton)
+    {
+        button.enabled=1
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
